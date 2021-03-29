bdb53662956b03ecc77839f2662a02312b94b1e1
The Altamont Enterprise <61389274+mae1884@users.noreply.github.com>
2021-03-28 19:09:35 -0500

    commit on 'master'

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -11,6 +11,6 @@ module.exports = {
 
     shortTitle: `Ghost`, // Used for App manifest e.g. Mobile Home Screen
     siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
-    backgroundColor: `#e9e9e9`, // Used for Offline Manifest
+    backgroundColor: `#e1e9e9`, // Used for Offline Manifest
     themeColor: `#15171A`, // Used for Offline Manifest
 }
