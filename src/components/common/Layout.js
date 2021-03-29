a177c78d4cf04e4052ea8523eb7cf4cb4d853cf6
mae1884 <miaia@altamontenterprise.com>
2021-03-29 15:29:52 -0500

    commit on 'master'

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -79,7 +79,7 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                     <footer className="site-foot">
                         <div className="site-foot-nav container">
                             <div className="site-foot-nav-left">
-                                <Link to="/">{site.title}</Link> © 2021 &mdash; Published with <a className="site-foot-nav-item" href="https://ghost.org" target="_blank" rel="noopener noreferrer">Ghost</a>
+                                <Link to="/">{site.title}</Link> © 2021 &mdash; Published with <a className="site-foot-nav-item" href="https://altamontenterprise.com" target="_blank" rel="noopener noreferrer">principle</a>
                             </div>
                             <div className="site-foot-nav-right">
                                 <Navigation data={site.navigation} navClass="site-foot-nav-item" />
